b02a65f645740ed8fa7b19b4a6bf39f3c4a74768
sdrissil <sara.drissilahssini@gmail.com>
2016-04-28 17:33:44 +0100

    correction de la lecture des enreggistrements depuis le fichier

--- longstat.sh
+++ longstat.sh
@@ -16,7 +16,7 @@
 	#Si le premier paramètre existe, on suppose que c'est le nom du fichier
 	if [ $# -eq 1 ]
 	then
-		echo "Vous avez choisi un seul paramètre : $1"
+		echo "Vous avez saisi un seul paramètre : $1"
 		#Vérfier l'existence du fichier
 			if [ -e "$1" ]
 			then
@@ -24,10 +24,12 @@
 				mots=()
 				#Lire fichier et charger chaque mots dans le tableau
 				# le -r permet de ne pas interpreter les blancs
-				while IFS='' read -r line || [[ -n "$line" ]]; do
+				filename="$1"
+				while read "line" 
+				do
 					echo "Text read from file: $line"
 					mots+=("$line")
-				done < "$1"
+				done < "$filename"
 				
 				 # pour chaque lettre dans l'alphabet
 				for lettre in {a..z}
