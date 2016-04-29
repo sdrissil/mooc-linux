fdc90c8533ede9474c8d5bf79f5366f438d207e0
sdrissil <sara.drissilahssini@gmail.com>
2016-04-29 15:01:06 +0100

    traitment du deuxieme param + consolidation

--- longstat.sh
+++ longstat.sh
@@ -9,14 +9,17 @@
 #			(2) nom du fichier en sortie : Facultatif
 ###########################################################################################
 
-# Créer un tableau qui recevera le resultat des statistiques
-stats_triees=()
 
+# Génère les statiques sur l'utilisation des lettres
+# Paramètre : nom du dictionnaire
 generer_stats()
 {   
-        # Créer un tableau qui recevera le resultat des statistiques
-        stats=()
-        # Créer un tableau qui recevera la liste des mots du dictionnaire
+	#Vérfier l'existence du fichier
+	if [ -e "$1" ]
+	then
+		# Créer un tableau qui recevera le resultat des statistiques
+		stats=()
+		# Créer un tableau qui recevera la liste des mots du dictionnaire
 		mots=()
 		#Lire fichier et charger chaque mots dans le tableau
 		# le -r permet de ne pas interpreter les blancs
@@ -29,60 +32,55 @@ generer_stats()
 		# pour chaque lettre dans l'alphabet
 		for lettre in {A..Z}
 			do
-		    	#Initialisation de la variable occurence
-	        	declare -i occurence=0
+				#Initialisation de la variable occurence
+				declare -i occurence=0
 				# pour chaque entrée dans le tableau
 					for mot in "${mots[@]}"
-				    	do
-				    	    # Verifier que le mot contient la lettre
-						    if [[ $mot =~ .*$lettre.* ]]
+						do
+							# Verifier que le mot contient la lettre
+							if [[ $mot =~ .*$lettre.* ]]
 							   then
 									let "occurence=occurence+1"
 							fi
 						done
 				stats+=("$occurence - $lettre \n")
-	    	done
-	    	
-	    #Tri des stats
-	    stats_triees=( $(
-        for el in "${stats[@]}"
-        do
-            echo "$el"
-        done | sort -k1,1nr -k2,2) )
+			done
+			
+		#Tri des stats
+		stats_triees=( $(
+		for el in "${stats[@]}"
+		do
+			echo "$el"
+		done | sort -k1,1nr -k2,2) )
+	else	
+		echo "Une erreur est survenue. Le fichier mentionné n'existe pas."
+	fi
 }
 
 
 # Vérifier les paramètres d'entrée
 	if [ $# -eq 1 ]
-	#Si le premier paramètre existe, on suppose que c'est le nom du fichier
 	then
-		echo "Vous avez saisi un seul paramètre : $1"
-		#Vérfier l'existence du fichier
-			if [ -e "$1" ]
-			then
-			    #Générer les stats pour le fichier donné
-				generer_stats $1
-                
-                #Afficher les résultats
-			    for stat in ${stats_triees[@]}; do printf $stat; done
-			else	
-				echo "Une erreur est survenue. Le fichier mentionné n'existe pas."
-			fi
+		#Si le premier paramètre existe, on suppose que c'est le nom du fichier
+		echo "Vous avez saisi un seul paramètre : $1"	
+		#Générer les stats pour le fichier donné
+		generer_stats $1               
+		#Afficher les résultats
+		for stat in ${stats_triees[@]}; do printf $stat; done
+			
 	elif [ $# -eq 2 ]
 	then 
 		#Si deux paramètres
 		echo "Vous avez saisi deux paramètres : $1 et $2"
 		#Générer les stats pour le fichier donné
-		generer_stats $1
-                
+		generer_stats $1               
 		for stat in ${stats_triees[@]}
 		do 
 	    	#Afficher les résultats
 		    printf $stat
 		    #Stocker les résultats le deuxième fichier
 		    printf "$stat" >> $2
-		done
-		
+		done	
 
 	elif [ $# -eq 0 ]
 	then 
